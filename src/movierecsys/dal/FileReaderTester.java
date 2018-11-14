5661d810b98c49f087e48eff01b48e8bca4763cb
CasperLC <43999061+CasperLC@users.noreply.github.com>
2018-11-14 12:20:08 +0100

    Working MovieDAO+Exercise1+ChangesFromPeter

--- src/movierecsys/dal/FileReaderTester.java
+++ src/movierecsys/dal/FileReaderTester.java
@@ -25,17 +25,6 @@ public class FileReaderTester
     public static void main(String[] args) throws IOException
     {
         MovieDAO movieDao = new MovieDAO();
-//        movieDao.createMovie(2222, "Test Movie");
-//        movieDao.createMovie(2223, "Virker vores");
-//        movieDao.createMovie(2224, "getNextAvailableId method?");
-//        movieDao.deleteMovie(new Movie(17764,1998,"Shakespeare in Love"));
-//        movieDao.createMovie(1998, "Shakespeare in Love");
-//        movieDao.deleteMovie(new Movie(17769,2003,"The Company"));
-//        movieDao.createMovie(2003, "The Company");
-//        movieDao.sortMovies();
-//        movieDao.updateMovie(new Movie(17769, 2003, "The Company"));
-//        movieDao.updateMovie(new Movie(17771, 2003, "The Company: HELLO"));
-//        movieDao.deleteMovie(new Movie(17771, 2003, "The Company: HELLO"));
         List<Movie> allMovs = movieDao.getAllMovies();
 
         for (Movie allMov : allMovs)
@@ -46,8 +35,6 @@ public class FileReaderTester
         System.out.println("next id " +movieDao.getNextAvailableId());
         System.out.println("The movie you requested is: " + movieDao.movieToString(movieDao.getMovie(17764)));
         
-
-//        System.out.println("Movie count: " + allMovs.size());
     }
     
    
